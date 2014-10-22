551b6f48662be4947adc89165e0c64a6a7c6a94a
Max Shmelev <Max.Shmelev@bellmedia.ca>
2014-10-22 13:38:35 -0400

    Initial commit

---
+++ RouteMPrefixAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace System.Web.Http
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+	public class RouteMPrefixAttribute : RoutePrefixAttribute
+	{
+		public RouteMPrefixAttribute(string prefix)
+			: this(prefix, 0)
+		{
+		}
+
+
+		public RouteMPrefixAttribute(string prefix, int order)
+			: base(prefix)
+		{
+			Order = order;
+		}
+
+		
+		public int Order { get; set; }
+	}
+}
